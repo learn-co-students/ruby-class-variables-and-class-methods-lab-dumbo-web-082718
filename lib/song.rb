b69631fbb7a5dec88fd320628796e19789af73e8
Gavin Soko <sokolofg@gmail.com>
2018-09-03 15:30:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,16 @@
 class Song 
-  attr_accessor :name, :title, :artist
+  attr_accessor :name, :title, :genre 
+  
+  @@count = 0
+  def initialize(name, title, genre) 
+    @name = name
+    @title = title
+    @genre = genre
+  end
+  
+  def artist
+    Song.title = @name
+    @name
+  end
+  
 end
