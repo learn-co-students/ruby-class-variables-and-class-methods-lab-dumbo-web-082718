e1738ee35ab83d42af5065f1b83ae1d646561951
Gavin Soko <sokolofg@gmail.com>
2018-09-04 14:52:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,69 @@
 class Song 
-  attr_accessor :name, :title, :genre 
+  attr_accessor :name, :artist, :genre 
   
   @@count = 0
-  def initialize(name, title, genre) 
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre) 
     @name = name
-    @title = title
+    @artist = artist
     @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
   end
   
-  def artist
-    Song.title = @name
-    @name
+  def self.artists
+    @@artists.uniq = self
   end
+  
+  def self.genres
+    @@genres.uniq = self
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    return @@artists.uniq
+  end
+  
+  def self.genres
+    return @@genres.uniq!
+  end
+  
+   def self.genre_count #go through each song and record/count each genre
+                        # need to create an empty hash 
+                        #iterate through @@songs
+                        #if genre is not already on the hash, add it to hash and set it to 1
+                        #if it is already there, increment the value
+   new_hash = {}
+   @@genres.each do |genre|
+     if new_hash[genre]
+     new_hash[genre] += 1
+     else 
+       new_hash[genre] = 1
+     end
+   end
+   return new_hash
+  end 
+  
+  def self.artist_count
+    #go through the artists array and count how many times each artist appears
+    #create an empty hash
+    # take each artist and add it to the hash
+    # if an artist is already in the hash increment it's value
+    #if its not in the hash already, add it and set it = 1
+    artist_hash = {}
+    @@artists.each do |artist|
+      if artist_hash[artist]
+        artist_hash[artist] += 1
+      else artist_hash[artist] = 1 
+    end
+  end
+  return artist_hash
+end
   
 end
